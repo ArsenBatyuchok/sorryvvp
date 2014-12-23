599ebe149d4cd23744336100290ee98c509c9a3a
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2014-12-23 12:34:41 -0600

    deleted .idea from rm

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-	var putinLastDay = new Date();
-	putinLastDay = new Date(putinLastDay.getFullYear() + 1, 1 - 1, 1);
-	$('#putinCountdown').countdown({until: putinLastDay, padZeroes: true});
+	var offset = 30;
+	var putinLastDay = new Date(2015, 0, 1, 0, -offset);
+	$('#putinCountdown').countdown({until: $.countdown.UTCDate(+3, putinLastDay), padZeroes: true});
 });
